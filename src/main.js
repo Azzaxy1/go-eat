efa24b6e61dfc57df5d813980412c4852c1dcb2e
Azzaxy <abdurrohmanazis@gmail.com>
2024-11-17 18:25:43 +0700

    first commit: setup environment

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import { BootstrapVue3 } from 'bootstrap-vue-3'
+
+const app = createApp(App)
+
+app.use(BootstrapVue3)
+app.use(router)
+
+app.mount('#app')
---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <div>
+      <b-button variant="danger">Primary Button</b-button>
+    </div>
+    <h1 va>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
