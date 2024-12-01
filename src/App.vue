0ab83ff4927d8941575e436fcbf2501f45243acb
Azzaxy <abdurrohmanazis@gmail.com>
2024-12-02 05:09:00 +0700

    feat: add footer and change favicon

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <Navbar />
   <RouterView />
+  <Footer />
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
     Navbar,
+    Footer,
   },
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-success text-white py-3 text-center">
+    <h5 class="mt-2 fs-6">
+      GoEat © {{ new Date().getFullYear() }};
+      <a href="https://github.com/Azzaxy1" target="_blank" class="text-white">Abdurrohman Azis</a>
+    </h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterSection',
+}
+</script>
